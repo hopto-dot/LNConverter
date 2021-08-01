f12c1ae40c7cf537448df88eb551f7cee070e0ff
George C <george.cowling@hotmail.com>
2021-08-01 21:46:23 +0100

    Add project files.

---
+++ LNConverter/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace LNConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.tbxFilePath = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.Location = new System.Drawing.Point(13, 5);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(127, 29);
+            this.btnSelectFile.TabIndex = 0;
+            this.btnSelectFile.Text = "Select Epub";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+            this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
+            // 
+            // tbxFilePath
+            // 
+            this.tbxFilePath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbxFilePath.Location = new System.Drawing.Point(148, 6);
+            this.tbxFilePath.Name = "tbxFilePath";
+            this.tbxFilePath.Size = new System.Drawing.Size(473, 27);
+            this.tbxFilePath.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(13, 41);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(127, 29);
+            this.btnConvert.TabIndex = 2;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblTitle.Location = new System.Drawing.Point(13, 80);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(608, 30);
+            this.lblTitle.TabIndex = 3;
+            this.lblTitle.Text = "[title]";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 122);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(170, 237);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(633, 367);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.tbxFilePath);
+            this.Controls.Add(this.btnSelectFile);
+            this.MinimumSize = new System.Drawing.Size(400, 266);
+            this.Name = "Form1";
+            this.Text = "LNConverter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.TextBox tbxFilePath;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ LNConverter/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using VersOne.Epub;
+
+namespace LNConverter
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblTitle.Text = "";
+        }
+
+        String FilePath = "";
+        EpubBook Epub;
+        String HTMLConvert = "";
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog OpenFile = new OpenFileDialog();
+            OpenFile.RestoreDirectory = true;
+            OpenFile.Filter = "Epub Files|*.epub";
+            if (OpenFile.ShowDialog() == DialogResult.OK)
+            {
+                FilePath = OpenFile.FileName;
+                tbxFilePath.Text = FilePath;
+                //Epub = EpubReader.ReadBook(FilePath);
+                try
+                {
+                    Epub = EpubReader.ReadBook(FilePath);
+                    
+                } catch
+                {
+                    MessageBox.Show("File \"kobo.js\" was not found in the archive.", "EPUB parsing error");
+                    return;
+                    //EpubBookRef epubBook = EpubReader.OpenBook(FilePath);
+                }
+                
+                lblTitle.Text = Epub.Title;
+
+                byte[] coverImageContent = Epub.CoverImage;
+                if (coverImageContent != null)
+                {
+                    using (MemoryStream coverImageStream = new MemoryStream(coverImageContent))
+                    {
+                        Image coverImage = Image.FromStream(coverImageStream);
+                        pictureBox1.Image = coverImage;
+                    }
+                }
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            FilePath = tbxFilePath.Text;
+            try
+            {
+                Epub = EpubReader.ReadBook(FilePath);
+            } catch
+            {
+                MessageBox.Show("Failed to convert EPUB to HTML", "Conversion Failed");
+                return;
+            }
+
+            int Index = 0;
+            foreach (EpubTextContentFile htmlContent in Epub.ReadingOrder)
+            {
+                string HTML = htmlContent.Content;
+                if (Index == 0)
+                {
+                    HTMLConvert = HTML;
+                }
+                else if (Index == Epub.ReadingOrder.Count - 1) {
+                    int SnipIndex = HTML.IndexOf("<html");
+                    HTML = HTML.Substring(SnipIndex);
+                    SnipIndex = HTML.IndexOf(">") + 4;
+                    HTML = HTML.Substring(SnipIndex);
+                }
+                else
+                {
+                    int SnipIndex = HTML.IndexOf("<html");
+                    HTML = HTML.Substring(SnipIndex);
+                    SnipIndex = HTML.IndexOf(">") + 4;
+                    HTML = HTML.Substring(SnipIndex);
+
+                    HTML = HTML.Replace("</html>", "");
+                    HTML = HTML.Trim();
+
+                    HTMLConvert += HTML;
+                }
+                
+                Index++;
+            }
+            HTMLConvert = HTMLConvert.Replace("\n", "");
+            File.Create(System.Environment.ExpandEnvironmentVariables("%userprofile%/downloads/") + Epub.Title + ".html").Dispose();
+            using (StreamWriter writer = new StreamWriter(System.Environment.ExpandEnvironmentVariables("%userprofile%/downloads/") + Epub.Title + ".html"))
+            {
+                writer.WriteLine(HTMLConvert);
+            }
+            MessageBox.Show("New file named " + Epub.Title + ".html" + " has been created in the downloads folder", "Conversion Successful");
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.Size.Height >= 650)
+            {
+                pictureBox1.Width = 340;
+                pictureBox1.Height = 474;
+            }
+            else if (this.Size.Height <= 400)
+            {
+                pictureBox1.Width = 170 / 2;
+                pictureBox1.Height = 237 / 2;
+            }
+            else
+            {
+                pictureBox1.Width = 170;
+                pictureBox1.Height = 237;
+            }
+        }
+    }
+}
+
